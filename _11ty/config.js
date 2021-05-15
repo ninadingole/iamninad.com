4fc23e839c06dfc30da726508a27829b48b81a68
Neenad Ingole <needna78@gmail.com>
2021-05-15 12:21:00 +0530

    refactor code for nunjucks filter and sortcode

--- _11ty/config.js
+++ _11ty/config.js
@@ -5,26 +5,26 @@ const markdownIt = require("markdown-it");
 const markdownItAnchor = require("markdown-it-anchor");
 const markdownItEmoji = require('markdown-it-emoji');
 const path = require('path');
-const { minify } = require('terser');
 const sitemap = require("@quasibit/eleventy-plugin-sitemap");
 const readingTime = require('eleventy-plugin-reading-time');
 const pluginSEO = require("eleventy-plugin-seo");
-const Image = require("@11ty/eleventy-img");
 
 
-const filters = require('./filters');
-const shortcodes = require('./shortcodes');
+const globalFilters = require('./filters');
+const globalShortCodes = require('./shortcodes');
+const nunjucksShortCodes = require('../src/_includes/nunjucks/sortcodes');
+const nunjucksFilters = require('../src/_includes/nunjucks/filters');
 const site = require('../src/_data/site');
 
 module.exports = function (eleventyConfig) {
     // Filters
-    Object.keys(filters).forEach(filterName => {
-        eleventyConfig.addFilter(filterName, filters[filterName]);
+    Object.keys(globalFilters).forEach(filterName => {
+        eleventyConfig.addFilter(filterName, globalFilters[filterName]);
     });
 
     // Shortcodes
-    Object.keys(shortcodes).forEach(shortcodeName => {
-        let val = shortcodes[shortcodeName];
+    Object.keys(globalShortCodes).forEach(shortcodeName => {
+        let val = globalShortCodes[shortcodeName];
         let fn = val.isPaired ? 'addPairedShortcode' : 'addShortcode';
         eleventyConfig[fn](shortcodeName, val.fn);
     });
@@ -35,65 +35,6 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPlugin(lazyImagesPlugin, {
         transformImgPath: src => isAbsolutePath(src) ? src : path.join(__dirname, '../src/_includes', src)
     });
-
-    // Collections
-    const livePosts = post => post.date <= new Date() && !post.data.draft;
-    eleventyConfig.addCollection('posts', collection => {
-        return collection.getFilteredByGlob('**/posts/**/*.md').filter(livePosts).reverse();
-    });
-
-    // Transforms
-    eleventyConfig.addTransform('htmlmin', filters.htmlmin);
-
-    /* Markdown Overrides */
-    let markdownLibrary = markdownIt({
-        html: true,
-        breaks: true,
-        linkify: true
-    }).use(markdownItAnchor, {
-        permalink: true,
-        permalinkBefore: true,
-        permalinkSymbol: ""
-    }).use(markdownItEmoji);
-
-    eleventyConfig.setLibrary("md", markdownLibrary);
-
-    eleventyConfig.addNunjucksAsyncFilter('jsmin', async function( code, callback) {
-        try {
-            const minified = await minify(code);
-            callback(null, minified.code);
-        } catch (err) {
-            console.error('Terser error: ', err);
-            callback(null, code);
-        }
-    });
-
-    eleventyConfig.addNunjucksAsyncShortcode("resI", async (src, alt, widths) => {
-        let metadata = await Image(src, {
-            widths: widths,
-            formats: ["webp", "jpeg"],
-            urlPath: '/assets/images',
-            outputDir: 'www/assets/images',
-            useCache: false,
-            cacheOptions: {
-                duration: '1d',
-                directory: 'www/.cache',
-                    removeUrlQueryParams: false,
-
-            }
-        });
-
-        let imageAttributes = {
-            alt,
-            sizes: widths,
-            loading: "lazy",
-            decoding: "async",
-        };
-
-        // You bet we throw an error on missing alt in `imageAttributes` (alt="" works okay)
-        return Image.generateHTML(metadata, imageAttributes);
-    });
-
     eleventyConfig.addPlugin(sitemap, {
         sitemap: {
         hostname: "https://iamninad.com",
@@ -112,6 +53,30 @@ module.exports = function (eleventyConfig) {
         }
     });
 
+    // Collections
+    const livePosts = post => post.date <= new Date() && !post.data.draft;
+    eleventyConfig.addCollection('posts', collection => {
+        return collection.getFilteredByGlob('**/posts/**/*.md').filter(livePosts).reverse();
+    });
+
+    // Transforms
+    eleventyConfig.addTransform('htmlmin', globalFilters.htmlmin);
+
+    /* Markdown Overrides */
+    let markdownLibrary = markdownIt({
+        html: true,
+        breaks: true,
+        linkify: true
+    }).use(markdownItAnchor, {
+        permalink: true,
+        permalinkBefore: true,
+        permalinkSymbol: ""
+    }).use(markdownItEmoji);
+
+    eleventyConfig.setLibrary("md", markdownLibrary);
+
+    nunjucksFilters.add(eleventyConfig);
+    nunjucksShortCodes.add(eleventyConfig);
 
     eleventyConfig
         .addPassthroughCopy({ 'src/_includes/assets': 'assets' })
---
+++ src/_includes/nunjucks/sortcodes.js
@@ -0,0 +1,31 @@
+const Image = require("@11ty/eleventy-img");
+
+module.exports = {
+    add: (eleventyConfig) => {
+        eleventyConfig.addNunjucksAsyncShortcode("resI", async (src, alt, widths) => {
+            let metadata = await Image(src, {
+                widths: widths,
+                formats: ["webp", "jpeg"],
+                urlPath: '/assets/images',
+                outputDir: 'www/assets/images',
+                useCache: false,
+                cacheOptions: {
+                    duration: '1d',
+                    directory: 'www/.cache',
+                        removeUrlQueryParams: false,
+
+                }
+            });
+
+            let imageAttributes = {
+                alt,
+                sizes: widths,
+                loading: "lazy",
+                decoding: "async",
+            };
+
+            // You bet we throw an error on missing alt in `imageAttributes` (alt="" works okay)
+            return Image.generateHTML(metadata, imageAttributes);
+        });
+    }
+};
