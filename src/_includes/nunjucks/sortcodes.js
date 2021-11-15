012238b6d33bd29279af3b91304af09858953b9e
Neenad Ingole <needna78@gmail.com>
2021-11-15 19:11:02 +0100

    add comment to template

--- src/_includes/nunjucks/sortcodes.js
+++ src/_includes/nunjucks/sortcodes.js
@@ -25,7 +25,21 @@ module.exports = {
             };
 
             // You bet we throw an error on missing alt in `imageAttributes` (alt="" works okay)
-            return Image.generateHTML(metadata, imageAttributes);
+            let lowsrc = metadata.jpeg[0];
+            let highsrc = metadata.jpeg[metadata.jpeg.length - 1];
+
+            return `<picture>
+              ${Object.values(metadata).map(imageFormat => {
+                return `  <source type="${imageFormat[0].sourceType}" srcset="${imageFormat.map(entry => entry.srcset).join(", ")}" sizes="100vw">`;
+              }).join("\n")}
+                <img
+                  src="${lowsrc.url}"
+                  width="${highsrc.width}"
+                  height="${highsrc.height}"
+                  alt="${alt}"
+                  loading="lazy"
+                  decoding="async">
+              </picture>`;
         });
     }
 };
